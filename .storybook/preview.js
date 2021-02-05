a5b199a67b829cb6b3185840770abac4ba027bf8
Fabien Marie-Louise <f.marie-louise@MacBook-Pro-de-Fabien-GIP.local>
2021-02-05 15:32:14 +0100

    Initial commit

---
+++ .storybook/preview.js
@@ -0,0 +1,11 @@
+/*
+// If your project is a library 
+
+import { app } from "@storybook/vue3";
+import MyLib from "..."
+app.use(MyLib)
+*/
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" }
+};
